4ad706e055b2a4217d964d77e6b11d1ff04ffc30
Budai Jozsef <bujoci@gmail.com>
2021-06-19 22:12:52 +0200

    1. házi feladat frankeinstein megoldás

--- python-homework/charsandord.py
+++ python-homework/charsandord.py
@@ -1,12 +1,14 @@
 
 start = ord("a")
 end = ord("z")
-i = 1
+code_matrix = ""
 
-while start <= end:
-    print (f"{chr(start)} {start} ", end =''),
-    if i % 3 == 0:
-        print("\n")
-    i += 1
-    start += 1
-#
+for i in range(start, end):
+    if i < start + 10 and i+20 < end+1:
+        code_matrix = code_matrix + chr(i) + " " + str(i) + " " + chr(i + 10) + " " + str(i + 10) + " " + chr(i + 20) + " " + str(i + 20) + "\n"
+    else:
+        code_matrix = code_matrix + chr(i) + " " + str(i) + " " + chr(i + 10) + " " + str(i + 10) + " \n"
+    if i == start + 9:
+        break
+
+print(code_matrix)
