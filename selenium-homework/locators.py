8b5bd6772101b5bcb4ef06df71ed839ea69b1aeb
Budai Jozsef <bujoci@gmail.com>
2021-06-28 01:06:09 +0200

    013 Feladat: Lokátorok gyakorlása - megoldva

---
+++ selenium-homework/locators.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+browser.get("http://localhost:9999/kitchensink.html")
+
+elem1 = browser.find_element_by_id("multiple-select-example")
+print(f"A(z) {elem1.tag_name} típusú elemben található szöveg: \n{elem1.text}")
+print("-"*50)
+
+elem2 = browser.find_element_by_name("enter-name")
+print(f"A {elem2.location} lokáción található {elem2.tag_name} típusú elem placeholder szövege: ")
+print(elem2.get_attribute("placeholder"))
+print("-"*50)
+
+elem3 = browser.find_element_by_id("hide-textbox")
+elem3.click()
+
+elem4 = browser.find_element_by_xpath("//input[@style='display: none;']")
+
+print(f"A {elem4.size} méretű rejtett elem típusa: {elem4.tag_name}")
+browser.quit()
