cfa811dec239ab2d0b6d5d64ffb80d002a92086e
Budai Jozsef <bujoci@gmail.com>
2021-07-16 19:23:09 +0200

    003 Feladat: Python CSV filekezelés - megoldva (javítás)

--- selenium2-homework/csvszures.py
+++ selenium2-homework/csvszures.py
@@ -6,15 +6,3 @@ with open('new.csv', 'w', encoding = 'utf-8') as new_csv:
 
         for i in nevek:
             new_csv.write(i[1].strip() + "," + i[0] + "\n")
-
-# with open('new.csv', 'w', encoding = 'utf-8') as new_csv
-#
-#     new_csv = csv.writer(n('table_in.csv', encoding = 'utf-8') as csvfile:
-#     nevek = csv.reader(csvfile, delimiter = ',')
-#     email_list = []
-#
-#     for i in nevek:
-#         print(i[0] + i[1])
-#
-# with open('new.csv', 'w', encoding = 'utf-8') as new_csv:
-#     new_csv = csv.writer(email_list)
